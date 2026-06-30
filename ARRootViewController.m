d0cf328df88262ab4f314d7304454b58d6b0e125
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 07:52:08 +0800

    Update ARRootViewController.m

--- ARRootViewController.m
+++ ARRootViewController.m
@@ -23,7 +23,6 @@
     self.tableView.separatorColor = [UIColor clearColor];
     self.tableView.showsVerticalScrollIndicator = NO;
     
-    // 初始化搜索框
     self.arSearchController = [[UISearchController alloc] initWithSearchResultsController:nil];
     self.arSearchController.searchResultsUpdater = self;
     self.arSearchController.obscuresBackgroundDuringPresentation = NO;
@@ -32,15 +31,15 @@
     self.definesPresentationContext = YES;
     self.navigationItem.hidesSearchBarWhenScrolling = NO;
 
-    // 右上角跳转 TG
-    UIBarButtonItem *tgItem = [[UIBarButtonItem alloc] initWithTitle:OBF("5447E9A291E98193") style:UIBarButtonItemStylePlain target:self action:@selector(arOpenTGChannel)]; // "TG频道"
+    // TG 频道按钮
+    UIBarButtonItem *tgItem = [[UIBarButtonItem alloc] initWithTitle:OBF("5447E9A291E98193") style:UIBarButtonItemStylePlain target:self action:@selector(arOpenTGChannel)];
     self.navigationItem.rightBarButtonItem = tgItem;
 
     [self loadInstalledApps];
 }
 
 - (void)arOpenTGChannel {
-    NSURL *url = [NSURL URLWithString:OBF("68747470733A2F2F742E6D652F696F7364756D707A7A7A")]; // "https://t.me/iosdumpzzz"
+    NSURL *url = [NSURL URLWithString:OBF("68747470733A2F2F742E6D652F696F7364756D707A7A7A")];
     [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
 }
 
@@ -52,29 +51,24 @@
 #pragma clang diagnostic pop
 
     NSMutableArray *validApps = [NSMutableArray array];
-    
     for (id proxy in apps) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-        NSString *bundleID = [proxy performSelector:NSSelectorFromString(OBF("62756E646C654964656E746966696572"))]; 
+        NSString *bundleIdStr = [proxy performSelector:NSSelectorFromString(OBF("62756E646C654964656E746966696572"))]; 
 #pragma clang diagnostic pop
-        if (!bundleID) continue;
+        if (!bundleIdStr) continue;
         
-        // 过滤系统 com.apple.
-        if ([bundleID hasPrefix:OBF("636F6D2E6170706C652E")]) continue;
+        if ([bundleIdStr hasPrefix:OBF("636F6D2E6170706C652E")]) continue;
         
-        // 🎯 核心判断: _TrollStore 文件必须在 .app 的【同级平级】目录
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
         NSURL *bundleURL = [proxy respondsToSelector:NSSelectorFromString(OBF("62756E646C6555524C"))] ? [proxy performSelector:NSSelectorFromString(OBF("62756E646C6555524C"))] : nil;
 #pragma clang diagnostic pop
         if (bundleURL) {
-            // stringByDeletingLastPathComponent 获取的是 .app 的上一级目录（即容器目录，也就是 .app 的同级目录）
+            // 完美判断 _TrollStore 文件在 .app 的同级(父)目录
             NSString *appParentDir = [bundleURL.path stringByDeletingLastPathComponent];
-            NSString *trollStorePath = [appParentDir stringByAppendingPathComponent:OBF("5F54726F6C6C53746F7265")]; // "_TrollStore"
-            if ([[NSFileManager defaultManager] fileExistsAtPath:trollStorePath]) {
-                continue; 
-            }
+            NSString *trollStorePath = [appParentDir stringByAppendingPathComponent:OBF("5F54726F6C6C53746F7265")];
+            if ([[NSFileManager defaultManager] fileExistsAtPath:trollStorePath]) continue; 
         }
         [validApps addObject:proxy];
     }
@@ -106,10 +100,10 @@
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
             SEL nameSel = NSSelectorFromString(OBF("6C6F63616C697A65644E616D65"));
             SEL bundleSel = NSSelectorFromString(OBF("62756E646C654964656E746966696572"));
-            NSString *bundleID = [proxy performSelector:bundleSel];
-            NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleID;
+            NSString *bundleIdStr = [proxy performSelector:bundleSel];
+            NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleIdStr;
 #pragma clang diagnostic pop
-            if ([name.lowercaseString containsString:searchText] || [bundleID.lowercaseString containsString:searchText]) {
+            if ([name.lowercaseString containsString:searchText] || [bundleIdStr.lowercaseString containsString:searchText]) {
                 [results addObject:proxy];
             }
         }
@@ -122,10 +116,10 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section { return 1; }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSString *cellId = OBF("41707043656C6C"); // "AppCell"
+    NSString *cellId = OBF("41707043656C6C"); 
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
     if (!cell) {
-        // Subtitle 样式，让包名在应用名正下方
+        // 包名在下方
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellId];
         cell.imageView.layer.masksToBounds = YES;
         cell.imageView.layer.cornerRadius = 12.0;
@@ -136,10 +130,10 @@
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
     SEL nameSel = NSSelectorFromString(OBF("6C6F63616C697A65644E616D65"));
     SEL bundleSel = NSSelectorFromString(OBF("62756E646C654964656E746966696572"));
-    NSString *bundleID = [proxy performSelector:bundleSel];
-    NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleID;
+    NSString *bundleIdStr = [proxy performSelector:bundleSel];
+    NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleIdStr;
     
-    NSString *version = @"-";
+    NSString *version = OBF("2D"); // "-"
     SEL versionSel = NSSelectorFromString(OBF("73686F727456657273696F6E537472696E67")); 
     if ([proxy respondsToSelector:versionSel]) {
         version = [proxy performSelector:versionSel];
@@ -149,26 +143,25 @@
     cell.textLabel.text = name;
     cell.textLabel.font = [UIFont systemFontOfSize:16 weight:UIFontWeightBold];
     
-    cell.detailTextLabel.text = bundleID; 
+    cell.detailTextLabel.text = bundleIdStr; 
     cell.detailTextLabel.font = [UIFont systemFontOfSize:12];
     cell.detailTextLabel.textColor = [UIColor systemGrayColor];
     
-    // 🎯 版本号通过 accessoryView 展示在最右边
+    // 版本号右侧对齐
     UILabel *rightVerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 75, 30)];
-    rightVerLabel.text = [NSString stringWithFormat:@"v%@", version];
+    rightVerLabel.text = [NSString stringWithFormat:OBF("762540"), version]; // "v%@"
     rightVerLabel.font = [UIFont systemFontOfSize:13 weight:UIFontWeightMedium];
     rightVerLabel.textColor = [UIColor secondaryLabelColor];
     rightVerLabel.textAlignment = NSTextAlignmentRight;
     cell.accessoryView = rightVerLabel; 
     
-    // 获取图标
     SEL iconSel = NSSelectorFromString(OBF("5F6170706C69636174696F6E49636F6E496D616765466F7242756E646C654964656E7469666965723A666F726D61743A7363616C653A"));
     if ([UIImage respondsToSelector:iconSel]) {
         NSMethodSignature *sig = [UIImage methodSignatureForSelector:iconSel];
         NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
         [inv setTarget:[UIImage class]];
         [inv setSelector:iconSel];
-        [inv setArgument:&bundleID atIndex:2];
+        [inv setArgument:&bundleIdStr atIndex:2];
         int format = 1;
         [inv setArgument:&format atIndex:3];
         CGFloat scale = [UIScreen mainScreen].scale;
@@ -214,17 +207,17 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
     SEL bundleSel = NSSelectorFromString(OBF("62756E646C654964656E746966696572"));
-    NSString *bundleID = [proxy performSelector:bundleSel];
+    NSString *bundleIdStr = [proxy performSelector:bundleSel];
     SEL nameSel = NSSelectorFromString(OBF("6C6F63616C697A65644E616D65"));
-    NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleID;
+    NSString *name = [proxy respondsToSelector:nameSel] ? [proxy performSelector:nameSel] : bundleIdStr;
 #pragma clang diagnostic pop
     
     UIAlertController *loading = [UIAlertController alertControllerWithTitle:OBF("E8AFB7E7A88DE58099") // "请稍候"
-                                                                     message:OBF("E6ADA3E59CA8E8AFB7E6B182E69C8DE58AA1E599A8E88EB7E58F96E78988E69CACE58897E8A1A82E2E2E") // "正在请求服务器获取版本列表..."
+                                                                     message:OBF("E6ADA3E59CA8E8AFB7E6B182E69C8DE58AA1E599A8E88EB7E58F96E78988E69CACE58897E8A1A82E2E2E")
                                                               preferredStyle:UIAlertControllerStyleAlert];
     [self presentViewController:loading animated:YES completion:nil];
     
-    [[ARDowngradeManager sharedManager] fetchTrackIDForBundleID:bundleID completion:^(long long trackId, NSError *error) {
+    [[ARDowngradeManager sharedManager] fetchTrackIDForBundleID:bundleIdStr completion:^(long long trackId, NSError *error) {
         if (error || trackId == 0) {
             [loading dismissViewControllerAnimated:YES completion:^{
                 UIAlertController *errAlert = [UIAlertController alertControllerWithTitle:OBF("E5A4B1E8B4A5") message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
@@ -234,7 +227,7 @@
             return;
         }
         
-        [[ARDowngradeManager sharedManager] fetchVersionsForTrackID:trackId completion:^(NSArray *versions, NSError *error) {
+        [[ARDowngradeManager sharedManager] fetchVersionsForTrackID:trackId completion:^(NSArray *versionsArr, NSError *error) {
             [loading dismissViewControllerAnimated:YES completion:^{
                 if (error) {
                     UIAlertController *errAlert = [UIAlertController alertControllerWithTitle:OBF("E5A4B1E8B4A5") message:error.localizedDescription preferredStyle:UIAlertControllerStyleAlert];
@@ -244,10 +237,10 @@
                 }
                 
                 ARVersionViewController *versionVC = [[ARVersionViewController alloc] init];
-                [versionVC setValue:bundleID forKey:OBF("62756E646C654944")];
-                [versionVC setValue:name forKey:OBF("6170704E616D65")];
-                [versionVC setValue:@(trackId) forKey:OBF("747261636B4944")];
-                [versionVC setValue:[versions sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:OBF("72656C656173655F64617465") ascending:NO]]] forKey:OBF("76657273696F6E73")];
+                versionVC.bundleID = bundleIdStr; 
+                versionVC.appName = name;
+                versionVC.trackID = trackId;
+                versionVC.versions = [versionsArr sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:OBF("72656C656173655F64617465") ascending:NO]]]; 
                 [self.navigationController pushViewController:versionVC animated:YES];
             }];
         }];

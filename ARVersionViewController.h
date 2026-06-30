6130eaf3d5d0a161e0b7266f82d7b12772bf75bb
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 05:59:40 +0800

    Update ARVersionViewController.h

--- ARVersionViewController.h
+++ ARVersionViewController.h
@@ -1,1 +1,9 @@
+// ARVersionViewController.h
+#import <UIKit/UIKit.h>
 
+@interface ARVersionViewController : UITableViewController
+@property (nonatomic, copy) NSString *bundleID;
+@property (nonatomic, copy) NSString *appName;
+@property (nonatomic, assign) long long trackID;
+@property (nonatomic, strong) NSArray *versions;
+@end

d541cf057d7a20eb239fc079a1773029200ecb4d
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 07:13:08 +0800

    Create Obfuscation.h

---
+++ Obfuscation.h
@@ -0,0 +1,37 @@
+// Obfuscation.h
+#ifndef Obfuscation_h
+#define Obfuscation_h
+
+#import <Foundation/Foundation.h>
+
+// 1. 视觉致盲：类名混淆
+#define ARAppDelegate           lIII1l1lI
+#define ARDowngradeManager      l1I1ll1lI
+#define ARRootViewController    II11ll11I
+#define ARVersionViewController I1I11l1lI
+
+// 2. 视觉致盲：私有属性与方法混淆
+#define installedApps           o_1lI1l
+#define filteredApps            o_l1I1I
+#define searchController        o_11llI
+#define allApps                 o_1I11l
+#define fetchTrackIDForBundleID o_lIl1I
+#define fetchVersionsForTrackID o_11I1l
+#define installAppWithTrackID   o_1l11l
+#define sharedManager           o_II11l
+
+// 3. 终极内存解密：Hex 转 String (替代明文字符串，干掉静态特征)
+static inline NSString * HEX_DEC(const char *hex) {
+    int len = (int)strlen(hex);
+    char *str = (char *)malloc(len / 2 + 1);
+    for(int i = 0; i < len; i += 2) {
+        char byte[3] = {hex[i], hex[i+1], 0};
+        str[i/2] = (char)strtol(byte, NULL, 16);
+    }
+    str[len/2] = '\0';
+    NSString *res = [NSString stringWithUTF8String:str];
+    free(str);
+    return res;
+}
+
+#endif /* Obfuscation_h */

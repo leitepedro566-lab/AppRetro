e8e25cfb56b3eea52d8db28820eca111b19b0744
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 05:58:15 +0800

    Update ARDowngradeManager.h

--- ARDowngradeManager.h
+++ ARDowngradeManager.h
@@ -1,1 +1,9 @@
+// ARDowngradeManager.h
+#import <Foundation/Foundation.h>
 
+@interface ARDowngradeManager : NSObject
++ (instancetype)sharedManager;
+- (void)fetchTrackIDForBundleID:(NSString *)bundleID completion:(void(^)(long long trackId, NSError *error))completion;
+- (void)fetchVersionsForTrackID:(long long)trackId completion:(void(^)(NSArray *versions, NSError *error))completion;
+- (void)installAppWithTrackID:(long long)trackId versionID:(long long)versionId bundleID:(NSString *)bundleID;
+@end

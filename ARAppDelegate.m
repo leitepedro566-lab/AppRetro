5e7a40f45781a95237ce5c44fd261d56d5b0d7a6
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 05:57:49 +0800

    Update ARAppDelegate.m

--- ARAppDelegate.m
+++ ARAppDelegate.m
@@ -1,1 +1,20 @@
+// ARAppDelegate.m
+#import "ARAppDelegate.h"
+#import "ARRootViewController.h"
 
+@implementation ARAppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor systemGroupedBackgroundColor];
+    
+    ARRootViewController *rootVC = [[ARRootViewController alloc] init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:rootVC];
+    
+    // 强制大标题配合药丸UI更美观
+    nav.navigationBar.prefersLargeTitles = YES;
+    
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end

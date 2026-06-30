9e04f3388593826913edf552cc9b3c3c8c4cb54c
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 07:35:11 +0800

    Update ARVersionViewController.m

--- ARVersionViewController.m
+++ ARVersionViewController.m
@@ -43,7 +43,6 @@
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
     CGFloat radius = 25.0;
     CACornerMask mask = kCALayerMinXMinYCorner | kCALayerMaxXMinYCorner | kCALayerMinXMaxYCorner | kCALayerMaxXMaxYCorner;
-
     cell.layer.borderWidth = 0.0;
     cell.layer.borderColor = [UIColor clearColor].CGColor;
     cell.layer.cornerRadius = radius;
@@ -57,13 +56,6 @@
             bg.strokeColor = [UIColor clearColor]; 
             bg.strokeWidth = 0.0;
             cell.backgroundConfiguration = bg;
-        }
-    } else {
-        if (cell.backgroundView) {
-            cell.backgroundView.layer.cornerRadius = radius;
-            cell.backgroundView.layer.maskedCorners = mask;
-            cell.backgroundView.layer.masksToBounds = YES;
-            cell.backgroundView.layer.borderWidth = 0.0;
         }
     }
     if (@available(iOS 13.0, *)) cell.layer.cornerCurve = kCACornerCurveContinuous;
@@ -82,7 +74,8 @@
     long long versionId = [ver[HEX_DEC("65787465726E616C5F6964656E746966696572")] longLongValue]; // "external_identifier"
     NSString *verStr = ver[HEX_DEC("62756E646C655F76657273696F6E")];
     
-    NSString *msg = [NSString stringWithFormat:HEX_DEC("E58DB3E5B086E5BC80E5A78BE99D99E9BB98E4B88BE8BDBDE5AE89E8A38520762540EFBC8CE5AE8CE68890E5908EE7B3BBE7BB9FE4BC9AE887AAE58AA8E5AE89E8A385E38082"), verStr]; // "即将开始静默下载安装 v%@..."
+    // "即将开始静默下载安装 v%@，完成后系统会自动更新安装。"
+    NSString *msg = [NSString stringWithFormat:HEX_DEC("E58DB3E5B086E5BC80E5A78BE99D99E9BB98E4B88BE8BDBDE5AE89E8A38520762540EFBC8CE5AE8CE68890E5908EE7B3BBE7BB9FE4BC9AE887AAE58AA8E69BB4E696B0E5AE89E8A385E38082"), verStr];
     
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:HEX_DEC("E7A1AEE8AEA4E9998DE7BAA7") // "确认降级"
                                                                    message:msg 
@@ -90,7 +83,18 @@
     
     [alert addAction:[UIAlertAction actionWithTitle:HEX_DEC("E58F96E6B688") style:UIAlertActionStyleCancel handler:nil]]; // "取消"
     [alert addAction:[UIAlertAction actionWithTitle:HEX_DEC("E5BC80E5A78BE9998DE7BAA7") style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) { // "开始降级"
+        // 1. 执行静默推量更新逻辑
         [[ARDowngradeManager sharedManager] installAppWithTrackID:self.trackID versionID:versionId bundleID:self.bundleID];
+        
+        // 2. 🎯 点击确认后，立即执行私有特权 API，实现完美的挂起并平滑自动返回桌面
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        id app = [NSClassFromString(HEX_DEC("55494170706C69636174696F6E")) performSelector:NSSelectorFromString(HEX_DEC("73686E617265644170706C69636174696F6E"))]; // UIApplication / sharedApplication
+        SEL suspSel = NSSelectorFromString(HEX_DEC("73757370656E64")); // "suspend"
+        if ([app respondsToSelector:suspSel]) {
+            [app performSelector:suspSel]; // 软件自动完美返回桌面
+        }
+#pragma clang diagnostic pop
     }]];
     
     [self presentViewController:alert animated:YES completion:nil];

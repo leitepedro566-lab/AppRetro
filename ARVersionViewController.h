a1720b0a9c2ec8fdaf164cb9825cdaa24dbaead9
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-01 08:19:10 +0800

    Update ARVersionViewController.h

--- ARVersionViewController.h
+++ ARVersionViewController.h
@@ -6,7 +6,5 @@
 @property (nonatomic, copy) NSString *appName;
 @property (nonatomic, assign) long long trackID;
 @property (nonatomic, strong) NSArray *versions;
-
-// 新增：接收从主页透传过来的物理路径
 @property (nonatomic, copy) NSString *appPhysicalPath; 
 @end

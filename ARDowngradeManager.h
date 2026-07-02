e0742237dcd68cd5de4804b39036cd2ac7c43597
leitepedro566-lab <leitepedro566@gmail.com>
2026-07-02 08:48:52 +0800

    Update ARDowngradeManager.h

--- ARDowngradeManager.h
+++ ARDowngradeManager.h
@@ -5,9 +5,11 @@
 + (instancetype)sharedManager;
 - (void)fetchTrackIDForBundleID:(NSString *)bundleID completion:(void(^)(long long trackId, NSError *error))completion;
 - (void)fetchVersionsForTrackID:(long long)trackId completion:(void(^)(NSArray *versions, NSError *error))completion;
+
+// 🎯 恢复原本的接口定义
 - (void)installAppWithTrackID:(long long)trackId versionID:(long long)versionId bundleID:(NSString *)bundleID;
 
-// 新增：账号合规验证与一键安全换号控制层接口
+// 账号合规验证与一键安全换号控制层接口
 - (void)verifyOwnershipForBundleID:(NSString *)bundleID appPath:(NSString *)appPath completion:(void(^)(BOOL isMatch, NSString *purchaser, NSString *active, NSArray *allAccounts))completion;
 - (void)executeAccountSwitchToName:(NSString *)targetName;
 @end
